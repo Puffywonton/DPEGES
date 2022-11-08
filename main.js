e554a240f48359f51c2865b1238e4d958df14c1b
PC wonton <wontonking123@outlook.com>
2022-11-08 14:03:17 +0100

    params core complete

--- main.js
+++ main.js
@@ -8,23 +8,56 @@ import DepGes from './DepGes'
 
 document.querySelector('#app').innerHTML = `
     <div id="params" class="params">
-        <div class="values-container">
-            <div class="dpe-container">
-                <span>valeur dpe</span>
-                <input type="text" value="12" id="dpeInput"></input>
+        <form>
+            <div class="values-container">
+                <div class="dpe-container">
+                    <span>valeur dpe</span>
+                    <input type="text" value="12" id="dpeInput"></input>
+                </div>
+                <div class="ges-container">
+                    <span>valeur ges</span>
+                    <input type="text" value="50" id="gesInput"></input>
+                </div> 
             </div>
-            <div class="ges-container">
-                <span>valeur ges</span>
-                <input type="text" value="50" id="gesInput"></input>
+            <div class="paramSelector">
+                <button id="presetParamBtn">Preset</button>
+                <button id="customParamBtn">Custom</button>
             </div>
-            <div class="size-container">
-                <span>width</span>
-                <input type="text" value="500" id="widthInput"></input>
-                <span>height</span>
-                <input type="text" value="600" id="heightInput"></input>
+            <div id="presetParamContainer">
+                <div id="stickerRatioContainer">
+                    <p>Veuillez choisir le format:</p>
+                    <input type="radio" id="stickerRatioSquare"
+                    name="ratio" value="square">
+                    <label for="stickerRatioSquare">square</label>
+                    <input type="radio" id="stickerRatioRectangle"
+                    name="ratio" value="rectangle">
+                    <label for="stickerRatioRectangle">rectangle</label>
+                </div>
+                <div id="stickerSizeContainer">
+                    <p>Veuillez choisir la taille:</p>
+                    <input type="radio" id="stickerSizeSmall"
+                    name="size" value="small">
+                    <label for="stickerSizeSmall">small</label>
+                    <input type="radio" id="stickerSizeMedium"
+                    name="size" value="medium">
+                    <label for="stickerSizeMedium">medium</label>
+                    <input type="radio" id="stickerSizeLarge"
+                    name="size" value="large">
+                    <label for="stickerSizeLarge">large</label>
+                </div>
+                <button id="createButton" class="create">create</button>
             </div>
-        </div>
-        <button id="createButton" class="create">create</button>
+            <div id="customParamContainer">
+                <div class="size-container">
+                    <span>width</span>
+                    <input type="text" value="500" id="widthInput"></input>
+                    <span>height</span>
+                    <input type="text" value="600" id="heightInput"></input>
+                </div>
+                <button id="createCustomButton" class="create">create</button>
+            </div>
+        </form>
+
         <div>
             <span>SCREENSHOT</span>
             <button id="screenshotButton">CLICK</button>
--- params.js
+++ params.js
@@ -2,13 +2,76 @@ import cepEges from "./cepEges"
 import DepGes from "./DepGes"
 
 export function params() { 
+    const presetParamBtn = document.getElementById("presetParamBtn")
+    const presetParamContainer = document.getElementById("presetParamContainer")
+    presetParamContainer.style.display = "none";
+    const customParamBtn = document.getElementById("customParamBtn")
+    const customParamContainer = document.getElementById("customParamContainer")
+    customParamContainer.style.display = "none";
+    presetParamBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        if (presetParamContainer.style.display === "none") {
+            presetParamContainer.style.display = "block"
+        } else {
+            presetParamContainer.style.display = "none"
+        }
+        if (customParamContainer.style.display === "block") {
+            customParamContainer.style.display = "none"
+        }
+    })
+    customParamBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        if (customParamContainer.style.display === "none") {
+            customParamContainer.style.display = "block"
+        } else {
+            customParamContainer.style.display = "none"
+        }
+        if (presetParamContainer.style.display === "block") {
+            presetParamContainer.style.display = "none"
+        }
+    })
     const createBtn = document.getElementById("createButton")
-    createBtn.addEventListener("click", (e)=> {
+    let ratio
+    let size
+    let stickerRatioContainer = document.getElementById("stickerRatioContainer")
+    stickerRatioContainer.addEventListener('change', function (e) {
+        ratio = e.target.value;
+        console.log(ratio)
+    });
+    let stickerSizeContainer = document.getElementById("stickerSizeContainer")
+    stickerSizeContainer.addEventListener('change', function (e) {
+        size = e.target.value;
+        console.log(size)
+    });
+    createBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        let dpeValue = document.getElementById('dpeInput').value
+        let gesValue = document.getElementById('gesInput').value
+        // let heightValue = document.getElementById('heightInput').value
+        // let widthValue = document.getElementById('widthInput').value
+        let heightValue = 600
+        let widthValue
+        if (size === "large") {
+            heightValue = heightValue * 1.2
+        }
+        if (size === "small") {
+            heightValue = heightValue * 0.8
+        }
+        if (ratio === "square") {
+            widthValue = heightValue 
+        }
+        if (ratio === "rectangle") {
+            widthValue = heightValue * 0.7
+        }
+        DepGes("DepGes", heightValue, widthValue, dpeValue, gesValue)
+    })
+    const createCustomBtn = document.getElementById("createCustomButton")
+    createCustomBtn.addEventListener("click", (e) => {
+        e.preventDefault()
         let dpeValue = document.getElementById('dpeInput').value
         let gesValue = document.getElementById('gesInput').value
         let heightValue = document.getElementById('heightInput').value
         let widthValue = document.getElementById('widthInput').value
-        // cepEges("cepeges", dpeValue, gesValue)
         DepGes("DepGes", heightValue, widthValue, dpeValue, gesValue)
     })
 }
