c5a9e18cfe95d482dbcd4da997646f4d0511fe6a
PC wonton <wontonking123@outlook.com>
2023-01-04 17:52:25 +0100

    fixes done

--- src/main.js
+++ src/main.js
@@ -14,48 +14,47 @@ document.querySelector('#app').innerHTML = `
                 <input type="text" value="50" id="gesInput"></input>
             </div>
         </div>
-        <span class="instructions">Choisissez entre des dimensions prédéfinies ou personalisées:</span>
         <div class="param-selector">
             <button class="param-selector-button" id="presetParamBtn">dimension prédéfinie</button>
             <button class="param-selector-button" id="customParamBtn">dimension personnalisée</button>
         </div>
         <div class="preset-param-container" id="presetParamContainer">
             <form>
-                <span class="instructions">Selectionnez le format:</span>
+                <span class="instructions">Format:</span>
                 <div class="sticker-ratio-container" id="stickerRatioContainer">
                     <input type="radio" id="stickerRatioSquare"
-                    name="ratio" value="square" required="required">
+                    name="ratio" value="square" required="required" checked="checked">
                     <label for="stickerRatioSquare">carré</label>
                     <input type="radio" id="stickerRatioRectangle"
                     name="ratio" value="rectangle">
                     <label for="stickerRatioRectangle">rectangle</label>
                 </div>
-                <span class="instructions">Selectionnez la taille:</span>
+                <span class="instructions">Taille:</span>
                 <div class="sticker-size-container" id="stickerSizeContainer">
                     <input type="radio" id="stickerSizeSmall"
-                    name="size" value="small" required="required">
+                    name="size" value="small" >
                     <label for="stickerSizeSmall">petit</label>
                     <input type="radio" id="stickerSizeMedium"
-                    name="size" value="medium">
+                    name="size" value="medium" required="required" checked="checked">
                     <label for="stickerSizeMedium">moyen</label>
                     <input type="radio" id="stickerSizeLarge"
                     name="size" value="large">
                     <label for="stickerSizeLarge">grand</label>
-                </div>
+                </div>               
                 <button id="createButton" class="create-button">générer</button>
             </form>
         </div>
         <div id="customParamContainer">
             <div class="custom-size-container">
-                <label for="widthInput">largeur</label>
-                <input type="text" value="500" id="widthInput"></input>
-                <label for="heightInput">hauteur</label>
-                <input type="text" value="600" id="heightInput"></input>
+                <label for="widthInput" class="instructions">Largeur: </label>
+                <input type="text" value="400" id="widthInput"></input>
+                <label for="heightInput" class="instructions">Hauteur: </label>
+                <input type="text" value="400" id="heightInput"></input>
             </div>
             <button id="createCustomButton" class="create-button">générer</button>
         </div>
     </div>
-    <div id="dpeGesDemoContainer" class="dpegesDemoContainer"></div>
+    <div id="dpeGesDemoContainer" class="dpeges-demo-container"></div>
     <button class="download-button" id="downloadButton">télécharger</button>
 `
 dpeGesDemoInit()
