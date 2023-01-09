8db9e56bf41cb25535757f62eee1d9c233c0b76c
PC wonton <wontonking123@outlook.com>
2023-01-09 22:32:54 +0100

    might need to remove stuff

--- src/dpeGesv2.js
+++ src/dpeGesv2.js
@@ -7,12 +7,13 @@ const isFocusChecker = (dpeValue, dpeMin, dpeMax, gesValue, gesMin, gesMax ) => 
     } 
 }
 
-const barLetterBuilder = (letter, classList, size) => {
+
+
+const barLetterBuilder = (letter, size) => {
     let barLetter = document.createElement("span")
     barLetter.innerHTML = letter
-    // barLetter.classList.add(classList);
-    // barLetter.style.fontSize = size + "px"
-    barLetter.style.fontSize = "10vw"
+    barLetter.classList.add("dpeGes-barLetter");
+    barLetter.style.fontSize = size + "vw"
     return(barLetter)
 }
 
@@ -22,6 +23,12 @@ const barsGenerator = (dpeValue, gesValue) => {
     let barBaseWidthSize = 15
     let barsContainer = document.createElement("div")
     barsContainer.classList.add("dpeGes-barsContainer")
+    let topLegend = document.createElement("div")
+    topLegend.style.width = "50%"
+    topLegend.style.height = "4%"
+    topLegend.style.backgroundColor = "red"
+    topLegend.innerHTML = "hello"
+    barsContainer.appendChild(topLegend)
     for (let data of datas) {
         barsContainer.append(barBuilder(
             barBaseWidthSize,
@@ -37,6 +44,12 @@ const barsGenerator = (dpeValue, gesValue) => {
         console.log(data.color, barBaseWidthSize)
         barBaseWidthSize = barBaseWidthSize + barBaseWidthIncrementor;
     }
+    let bottomLegend = document.createElement("div")
+    bottomLegend.style.width = "50%"
+    bottomLegend.style.height = "4%"
+    bottomLegend.style.backgroundColor = "red"
+    bottomLegend.innerHTML = "hello"
+    barsContainer.appendChild(bottomLegend)
     return(barsContainer)
 }
 
@@ -49,26 +62,27 @@ const barBuilder = (barBaseWidthSize, barColor, dpeValue, dpeMin, dpeMax, gesVal
     barBase.classList.add("dpeGes-barBase")
     barArrowContainer.appendChild(barBase)
     let barTipContainer = document.createElement("div")
-    barTipContainer.classList.add("dpeGes-barTipContainer")
     let barTip = document.createElement("div")
     barTip.classList.add("dpeGes-barTip")  
     barBase.style.backgroundColor = barColor
     barTip.style.backgroundColor = barColor
-    barBase.appendChild(barLetterBuilder(letter))
     if (isFocusChecker(dpeValue, dpeMin, dpeMax, gesValue, gesMin, gesMax)) {
         console.log("it is this one", letter)
         barContainer.style.height = "18%"
         barTipContainer.style.width = "36%"
-        barBase.style.width = (barBaseWidthSize - 10) + "%"
+        barBase.style.width = (barBaseWidthSize) + "%"
         let focusLegendContainer = document.createElement("div")
         focusLegendContainer.style.width = "45%"
         focusLegendContainer.style.backgroundColor = barColor
-        focusLegendContainer.style.borderRight = "1px solid"
         barContainer.appendChild(focusLegendContainer)
+        barBase.appendChild(barLetterBuilder(letter, 8))
+        barTipContainer.classList.add("dpeGes-barTipContainer-focus")
     } else {
+        barTipContainer.classList.add("dpeGes-barTipContainer")
         barBase.style.width = barBaseWidthSize + "%"
         barContainer.style.height = "12%"
         barTipContainer.style.width = "24%"
+        barBase.appendChild(barLetterBuilder(letter, 5))
     }
     barTipContainer.appendChild(barTip)
     barArrowContainer.appendChild(barTipContainer)
